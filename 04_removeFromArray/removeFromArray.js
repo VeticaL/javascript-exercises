ac78e4c2715a0c87d4216a835b441fbe0f2c3cbc
Vertical <ambintasso@gmail.com>
2022-12-07 11:16:47 +0530

    Added leap years

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
-
+    const array = args[1,2,3,4];
+    const item = 3
+    const newArray = [];
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push[item];
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
